0b73e7ea4389b0b307179b9f6b033a6c392bf359
Linda Gibson <dakidz@live.com>
2014-07-27 19:21:08 -0700

    refactor and simplify code

--- lib/card_draw.rb
+++ lib/card_draw.rb
@@ -2,10 +2,7 @@ def card_draw(score)
 	card_values = { "Ace" => 4096, "King" => 2048, "Queen" => 1024, "Jack" => 512, 10 => 256, 9 => 128, 8 => 64, 7 => 32, 6 => 16, 5 => 8, 4 => 4, 3 => 2, 2 => 1 }
 	card_drawn = []
 	card_values.each do |card, value|
-		if score == value
-			card_drawn.push(card)
-			score -= value
-		elsif score >= value
+		if score >= value
 			card_drawn.push(card)
 			score -= value
 		end
